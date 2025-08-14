1393e3410e41bd5f0de08f1177e87a70a3e8b21b
VictorLopesz <euvictor_h@hotmail.com>
2025-08-14 19:10:26 -0300

    manipulando campo de texto

--- web/cypress/e2e/consultancy.cy.js
+++ web/cypress/e2e/consultancy.cy.js
@@ -49,5 +49,11 @@ describe("Formulário de Consultoria", () => {
 
     cy.get('input[type="file"]')
       .selectFile('cypress/fixtures/teste.pdf', {force: true})
-  });
+
+    cy.get('textarea[placeholder="Descreva mais detalhes sobre sua necessidade"]')
+    .type("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.")
+
+    });
+
+
 });
